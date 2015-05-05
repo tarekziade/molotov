b40afd64b36c450ab3e0b266c4e562d189bcc69e
Tarek Ziade <tarek@ziade.org>
2015-05-05 11:03:48 +0200

    added example

--- ailoads/run.py
+++ ailoads/run.py
@@ -1,21 +1,8 @@
-from ailoads.fmwk import scenario, runner, requests
+from ailoads.fmwk import runner
 
 
-@scenario(5)
-def _scenario_one():
-    """Calls Google.
-    """
-    return requests.get('http://localhost:8000')
-
-
-@scenario(30)
-def _scenario_two():
-    """Calls Yahoo.
-    """
-    return requests.get('http://localhost:8000')
-
-
-if __name__ == '__main__':
+def main():
+    from ailoads import example
     res = runner(10, 60)
     tok, tfailed = 0, 0
 
@@ -25,3 +12,7 @@ if __name__ == '__main__':
 
     print('')
     print('%d OK, %d Failed' % (tok, tfailed))
+
+
+if __name__ == '__main__':
+    main()
