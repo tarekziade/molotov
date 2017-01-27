5646dd35e34fc8d22b0da7921d478fb5a662ab90
Tarek Ziade <tarek@ziade.org>
2017-01-27 09:40:54 +0100

    added a work() test

--- molotov/tests/test_fmwk.py
+++ molotov/tests/test_fmwk.py
@@ -1,6 +1,8 @@
 import asyncio
+from collections import namedtuple
+
 from molotov.session import LoggedClientSession
-from molotov.fmwk import step
+from molotov.fmwk import step, worker
 from molotov.api import scenario
 from molotov.tests.support import TestLoop, async_test
 
@@ -12,14 +14,36 @@ class TestFmwk(TestLoop):
         res = []
 
         @scenario(0)
-        def test_one(session):
+        async def test_one(session):
             res.append('1')
 
         @scenario(100)
-        def test_two(session):
+        async def test_two(session):
             res.append('2')
 
         stream = asyncio.Queue()
         async with LoggedClientSession(loop, stream) as session:
             await step(session, False, False, False, stream)
             self.assertEqual(len(res), 1)
+
+    @async_test
+    async def test_worker(self, loop):
+
+        @scenario(0)
+        async def test_one(session):
+            pass
+
+        @scenario(100)
+        async def test_two(session):
+            pass
+
+        results = {'OK': 0, 'FAILED': 0}
+        stream = asyncio.Queue()
+        args = namedtuple('args', 'verbose quiet duration exception')
+        args.verbose = True
+        args.quiet = False
+        args.duration = 1
+        args.exception = True
+
+        await worker(loop, results, args, stream)
+        self.assertTrue(results['OK'] > 0)
