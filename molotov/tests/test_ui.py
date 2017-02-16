b2d583d65cf2568bb4305c7e05ceb97ff7d71d4b
Tarek Ziade <tarek@ziade.org>
2017-02-16 10:05:22 +0100

    getting the curse screen some data

--- molotov/tests/test_ui.py
+++ molotov/tests/test_ui.py
@@ -1,17 +1,31 @@
 import unittest
+import time
+import os
+
 from molotov.ui import init_screen, quit
+from molotov.result import LiveResults
 
 
 class TestUI(unittest.TestCase):
 
     def test_init_screen(self):
+        results = LiveResults()
+
         called = []
-        pids = [1, 2, 3]
+        pids = [1, 2, 3, os.getpid()]
+        running = time.time()
 
         def get_results(*args):
             called.append(True)
-            quit()
+            # give the updater a chance to kick in
+            if time.time() - running > 2:
+                quit()
+            else:
+                results.incr_success()
+                results.incr_failure()
+
+            return results
 
         ui = init_screen(pids, get_results)
         ui.run()
-        self.assertEqual(called, [True])
+        self.assertTrue(len(called) > 1)
