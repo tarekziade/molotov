0aae4ac9773d61381618485dc6c7898cdce73c0a
Tarek Ziade <tarek@ziade.org>
2017-04-20 16:15:30 +0200

    flakes

--- molotov/tests/example4.py
+++ molotov/tests/example4.py
@@ -1,41 +1,53 @@
-from molotov import *
+import molotov
 
 
-@global_setup()
+class SomeObject(object):
+    """Does something smart in real lif with the async loop.
+    """
+    def __init__(self, loop):
+        self.loop = loop
+
+    def cleanup(self):
+        pass
+
+
+@molotov.global_setup()
 def init_test(args):
-    set_var('SomeHeader') = '1'
-    set_var('endpoint') = 'http://localhost:8080'
+    molotov.set_var('SomeHeader', '1')
+    molotov.set_var('endpoint', 'http://localhost:8080')
 
 
-@setup()
+@molotov.setup()
 async def init_worker(worker_num, args):
-    headers = {'AnotherHeader': '1', 'SomeHeader': get_var('SomeHeader')}
+    headers = {'AnotherHeader': '1',
+               'SomeHeader': molotov.get_var('SomeHeader')}
     return {'headers': headers}
 
 
-@setup_session()
+@molotov.setup_session()
 async def init_session(worker_num, session):
     session.ob = SomeObject(loop=session.loop)
 
 
-@scenario(100)
+@molotov.scenario(100)
 async def scenario_one(session):
-    async with session.get(get_var('endpoint')) as resp:
+    endpoint = molotov.get_var('endpoint')
+    async with session.get(endpoint) as resp:
         res = await resp.json()
         assert res['result'] == 'OK'
         assert resp.status == 200
 
 
-@teardown_session()
+@molotov.teardown_session()
 async def end_session(worker_num, session):
     session.ob.cleanup()
 
 
-@teardown()
+@molotov.teardown()
 def end_worker(worker_num):
     print("This is the end for %d" % worker_num)
 
 
-@global_teardown()
+@molotov.global_teardown()
 def end_test():
     print("This is the end of the test.")
