a5dca4d446773eb99cdc184d3cd380e22b358e70
Tarek Ziade <tarek@ziade.org>
2018-03-22 10:12:28 +0100

    allow multiple errors

--- molotov/tests/test_fmwk.py
+++ molotov/tests/test_fmwk.py
@@ -235,7 +235,9 @@ class TestFmwk(TestLoop):
 
         await w.run()
         output = await serialize(console)
-        self.assertTrue("nodename nor servname provided" in output, output)
+        expected = ("Name or service not known" in output or
+                    "nodename nor servname provided" in output)
+        self.assertTrue(expected, output)
 
     @async_test
     async def test_setup_session_fresh_loop(self, loop, console, results):
